e6afe8e6ee21b4c1cad32d14e7f9eaa4c77e9c28
Mohamed <mohamedabdelnasser061@gmail.com>
2026-04-04 21:11:35 +0200

    Add missing comment block to Global.h for clarity

--- Global.h
+++ Global.h
@@ -1,4 +1,7 @@
 #pragma once
+/*
+ * This file contains global variables and definitions for the application.
+ */
 #include <iostream>
 #include "Models/clsUser.h"
 clsUser CurrentUser = clsUser::Find("", "");
